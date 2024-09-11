500c182ea2357f62baf576a2302925d3e58e17a6
Ben West <b-m-s-west@outlook.com>
2024-09-11 21:37:39 +0100

    Update to map to have a better hover function and display correct data

--- app.py
+++ app.py
@@ -8,28 +8,40 @@ st.title("2024 Olympic Medlists Birth Locations")
 df = pd.read_csv('2024_medalists_all.csv')
 df_filtered = df.dropna(subset=['lat', 'lon'])
 
-medal_colours = {
-    'Gold': 'yellow',
-    'Silver':'silver',
-    'Bronze': 'brown"
+medal_colors = {
+    'gold': '#FFD700',
+    'silver':'#C0C0C0',
+    'bronze': '#CD7F32'
 }
 
 df_filtered['representing_country'] = "Representing: " + df_filtered["country_medal"]
+
+print(df_filtered.head())
 
 fig = px.scatter_geo(
     df_filtered,
     lat="lat",
     lon="lon",
     hover_name='medalist_name',
-    hover_data={'lat': False, 'lon': False, 'place_of_birth': True, 'Representing': True, 'event_name': True},
-    colour='medal',
-    color_discrete_map=medal_colours,
+    hover_data={'medal':True, 'lat': False, 'lon': False, 'place_of_birth': True, 'representing_country': True, 'event_name': True},
+    color='medal',
+    color_discrete_map=medal_colors,
     title="2024 Olympic Medlists Birth Locations",
     projection="natural earth",
     template="plotly_dark"
 )
 
-fig.update_traces(marker=dict(size=12))
+# Customize hover template
+fig.update_traces(
+    hovertemplate=(
+        "<b>%{hovertext}</b><br><br>" +
+        "Medal: %{customdata[0]}<br>" +
+        "Birth Place: %{customdata[3]}<br>" +
+        "Representing: %{customdata[4]}<br>" +
+        "Event: %{customdata[5]}<br>" +
+        "<extra></extra>"
+    )
+)
 
 fig.update_geos(
     showcoastlines=True,
